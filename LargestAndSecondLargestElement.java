7f834b6ca0f598bf6510a3a7ed3ffc6ea30b05fa
ChetanAavarkar <chetanaavarkar12@gmail.com>
2026-08-27 19:00:50 +0530

    Add files via upload

---
+++ LargestAndSecondLargestElement.java
@@ -0,0 +1,40 @@
+package dsa;
+
+import java.util.Scanner;
+
+public class LargestAndSecondLargestElement {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the number of elements: ");
+		int n = sc.nextInt();
+		
+		int[] arr = new int[n];
+		System.out.println("Enter " + n + " elements: ");
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		int largest = Integer.MIN_VALUE;
+		int secondLargest = Integer.MIN_VALUE;
+		
+		for (int num : arr) {
+			if (num > largest) {
+				secondLargest = largest;
+				largest = num;
+			} else if (num > secondLargest && num != largest) {
+				secondLargest = num;
+			}
+		}
+		
+		System.out.println("Largest element: " + largest);
+		
+		if (secondLargest == Integer.MIN_VALUE) {
+			System.out.println("Second largest element: Not found ");
+		} else {
+			System.out.println("Second largest element:" + secondLargest);
+		}
+		sc.close();
+	}
+}
