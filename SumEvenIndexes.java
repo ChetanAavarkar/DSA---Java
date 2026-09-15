e015e3928f6fbae066bbfddc2484c6c26e7750dd
ChetanAavarkar <chetanaavarkar12@gmail.com>
2026-09-15 18:46:05 +0530

    Add files via upload

---
+++ SumEvenIndexes.java
@@ -0,0 +1,27 @@
+package dsa;
+
+import java.util.Scanner;
+
+public class SumEvenIndexes {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter size of array: ");
+		int n = sc.nextInt();
+		
+		int[] arr = new int[n];
+		System.out.println("Enter " + n + " elements:");
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		int sum = 0;
+		for (int i = 0; i < n; i += 2) {
+			sum += arr[i];
+		}
+		
+		System.out.println("Sum of elements at even indexes = " + sum);
+		sc.close();
+	}
+}
